c26673256f4f11c2defe032684bdb95f0bcf9467
santostorresfabricio-max <santostorresfabricio@gmail.com>
2026-08-07 12:52:55 -0500

    Corrección de configuración para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio-santos/', // 👈 Esto le dice a Vite que busque los archivos en tu repositorio
 })
